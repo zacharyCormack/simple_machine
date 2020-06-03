3eda360df13594ab659fa3fec4e10f91bf1ce8a6
Zachary Runge Cormack <cormack.zachary@gmail.com>
2020-06-03 13:22:03 -0400

    fixed a mistake.

--- slant.cpp
+++ slant.cpp
@@ -9,4 +9,19 @@ void move_slant(Component::Instance* object) {
 	object->velocity.z[0] += object->velocity.z[1];
 	/* do stuff to core if necessary */
 };
-Component slant(draw_prism, slant_coefficients, &move_slant);
+
+void bind_slant_points(prism core) {
+	const unsigned short int size = 0;
+	/* make size something other than 0 */
+	float vertices[size];
+	/* decide points somehow */
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float), &vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 13, (void*)0);
+	glEnableVertexAttribArray(0);
+}
+
+unsigned short slant_indices[] = {
+	/* put indices here */
+};
+
+Component slant(draw_prism, slant_coefficients, &move_slant, &bind_slant_points, slant_indices);
