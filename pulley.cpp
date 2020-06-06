2b7551925e520b3f6a32ac8f7f52a789d2926b55
Zachary Runge Cormack <cormack.zachary@gmail.com>
2020-06-06 15:48:01 -0400

    updated react functions to be dependent on mass.

--- pulley.cpp
+++ pulley.cpp
@@ -2,12 +2,12 @@
 #include "forces.hpp"
 #include "shaders.hpp"
 
-double pulley_coefficients[] = {0.2, 0.0, 1.0, 0.0, 0.0};
+double pulley_coefficients[] = {0.2, 0.4, 1.0, 0.0, 0.0};
 
 void move_pulley(Component::Instance* object) {
-	object->velocity.x[0] += object->velocity.x[1];
-	object->velocity.y[0] += object->velocity.z[1];
-	object->velocity.z[0] += object->velocity.z[1];
+	object->velocity.x[0] += object->velocity.x[1]/0.4;
+	object->velocity.y[0] += object->velocity.z[1]/0.4;
+	object->velocity.z[0] += object->velocity.z[1]/0.4;
 	/* do stuff to core if necessary */
 };
 
--- rope.cpp
+++ rope.cpp
@@ -2,12 +2,12 @@
 #include "forces.hpp"
 #include "shaders.hpp"
 
-double rope_coefficients[] = {0.1, 0.0, 1.0, 3.5, 0.0};
+double rope_coefficients[] = {0.1, 0.15, 1.0, 3.5, 0.0};
 
 void move_rope(Component::Instance* object) {
-	object->velocity.x[0] += object->velocity.x[1];
-	object->velocity.y[0] += object->velocity.z[1];
-	object->velocity.z[0] += object->velocity.z[1];
+	object->velocity.x[0] += object->velocity.x[1]/0.15;
+	object->velocity.y[0] += object->velocity.z[1]/0.15;
+	object->velocity.z[0] += object->velocity.z[1]/0.15;
 	/* do stuff to core if necessary */
 };
 
