1418ec3bd0f41481816407d6159d4d8777232c35
Zachary Runge Cormack <cormack.zachary@gmail.com>
2020-06-07 17:06:28 -0400

    updated uvec function

--- input.cpp
+++ input.cpp
@@ -2,8 +2,7 @@
 
 double* act(angle ang, double coefficient) {
 	double force_vec[3] = {coefficient*0.5, 0, 0};
-	return line_x_y_x(vec_mult(uvec(ang), force_vec));
-	return force_vec;
+	return line_x_y_x(vec_mult(uvec(ang, (angle){(char)(0)}), force_vec));
 };
 
 Force input(act);
--- tension.cpp
+++ tension.cpp
@@ -2,7 +2,7 @@
 
 double* act_tension(angle ang, double coefficient) {
 	double force_vec[3] = {coefficient*3.5, 0, 0};
-	return line_x_y_x(vec_mult(uvec(ang), force_vec));
+	return line_x_y_x(vec_mult(uvec(ang, (angle){(char)(0)}), force_vec));
 };
 
 Force tension(act_tension);
